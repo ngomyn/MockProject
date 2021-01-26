2c907d1c510f30f38da5f86d1776c55aae415e0b
TrungPhuc98 <qwe_iop2006@yahoo.com>
2021-01-26 20:55:34 +0700

    v4.3.2

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -7,48 +7,40 @@ import { DataService } from '../../service/data.service'
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  navBarArray=[];
-  navBarObject={};
+  navBarCategory1=[];
+  navBarCategory2=[];
+  child1 : any
+  child2 : any
   constructor(
     private dataService:DataService
   ) { }
 
   ngOnInit(): void {
-    // this.test();
+    this.showCategory();
   }
-
-      
-  // showCategory(){
-  //   this.dataService.getData().subscribe(
-  //     data => {
-  //       this.navBarCategory1 = data.filter((item)=> item.parent_id == null);
-  //       this.navBarCategory2 = data.filter((item)=> item.parent_id)
-  //       this.navBarCategory1.forEach((item)=>{
-  //         item.child = []
-  //         this.navBarCategory2.forEach((i)=>{
-  //           if(i.parent_id == item._id){
-  //             item.child.push(i);
-  //           }
-  //         })
-  //         item.child.forEach((element) => {
-  //           element.children = []
-  //           this.navBarCategory2.forEach((x) => {
-  //             if(x.parent_id == element._id){
-  //               element.children.push(x);
-  //             }
-  //           })
-  //           this.child2 = element.children
-  //         })
-  //         this.child1 = item.child
-  //       })
-        
-  //       console.log(this.navBarCategory1);
-  //       console.log(this.navBarCategory2);
-        
-  //     }
-  //     console.log(data);
-  //   })
-  // }
-      
-  
+  showCategory(){
+    this.dataService.getData().subscribe(
+      data => {
+        this.navBarCategory1 = data.filter((item)=> item.parent_id == null);
+        this.navBarCategory2 = data.filter((item)=> item.parent_id)
+        this.navBarCategory1.forEach((item)=>{
+          item.child = []
+          this.navBarCategory2.forEach((i)=>{
+            if(i.parent_id == item._id){
+              item.child.push(i);
+            }
+          })
+          item.child.forEach((grand) => {
+            grand.child = []
+            this.navBarCategory2.forEach((x) => {
+              if(x.parent_id == grand._id){
+                grand.child.push(x);
+              }
+            })
+          })
+        })
+        console.log(this.navBarCategory1);
+      }
+    )
+  }
 }
